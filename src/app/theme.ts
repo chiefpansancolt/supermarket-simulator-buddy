7fbb74088180d59a354f045452b3c7807a8d5614
Christopher Pezza <chris.pezza@hey.com>
2025-12-05 15:00:03 -0500

    update theme

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -8,4 +8,30 @@ export const customTheme: CustomFlowbiteTheme = createTheme({
   dropdown: {
     inlineWrapper: "cursor-pointer",
   },
+  modal: {
+    header: {
+      close: {
+        base: "cursor-pointer",
+      },
+    },
+  },
+  select: {
+    field: {
+      select: {
+        base: "cursor-pointer",
+      },
+    },
+  },
+  sidebar: {
+    item: {
+      base: "cursor-pointer text-sm",
+    },
+  },
+  tabs: {
+    tablist: {
+      tabitem: {
+        base: "cursor-pointer",
+      },
+    },
+  },
 });
